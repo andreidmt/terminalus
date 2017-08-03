c90c080156be1e7bd5671fba858264f6cb86045c
Andrei Dumitrescu <andrei@codemachiner.com>
2017-08-04 00:24:19 +0200

    Moved things around

--- source/utils.js
+++ source/utils.js
@@ -1,14 +1,11 @@
+// @flow
+
 // const _debug = require( "debug" )( "Dashboard:Util" )
 const _chalk = require( "chalk" )
 const _boxen = require( "boxen" )
+const { spawn } = require( "child_process" )
+const M = require( "./m" )
 
-const COLORS = {
-    GRAY : "#f9f9f9",
-    PINK : "#da7b7b",
-    BLUE : "#7ba2f1",
-    RED  : "#dc322f",
-    GREEN: "#c3e88d",
-}
 
 const BOXEN_OPT = {
     padding: {
@@ -19,16 +16,92 @@ const BOXEN_OPT = {
     },
 }
 
-const infoText = _chalk.hex( COLORS.BLUE )
-const errorText = _chalk.red
-const successText = _chalk.green
+export type ChildProcessType = child_process$ChildProcess
 
 module.exports = {
-    COLORS,
-    infoText ,
-    errorText ,
-    successText,
-    infoBox   : input => infoText.bgBlack( _boxen( input , BOXEN_OPT ) ),
-    errorBox  : input => errorText.bgBlack( _boxen( input, BOXEN_OPT ) ),
-    successBox: input => successText.bgBlack( _boxen( input , BOXEN_OPT ) ),
+    COLORS: {
+        GRAY : "#f9f9f9",
+        PINK : "#da7b7b",
+        BLUE : "#7ba2f1",
+        RED  : "#dc322f",
+        GREEN: "#c3e88d",
+    },
+
+    infoText   : _chalk.blue,
+    errorText  : _chalk.red,
+    successText: _chalk.green,
+
+    infoBox: ( input: string ) =>
+        _chalk.blue.bgBlack( _boxen( input , BOXEN_OPT ) ),
+    errorBox: ( input: string ) =>
+        _chalk.red.bgBlack( _boxen( input, BOXEN_OPT ) ),
+    successBox: ( input: string ) =>
+        _chalk.green.bgBlack( _boxen( input , BOXEN_OPT ) ),
+
+    /**
+     * Start new child process, kill old one
+     *
+     * @param  {ChildProcessType}  current  Node child process
+     * @param  {string}        cmd      Process command string
+     * @param  {string[]}      args     Process arguments string
+     *
+     * @return {ChildProcessType}  Newly spawned child process
+     */
+    start: (
+        current: ?ChildProcessType,
+        cmd: string,
+        args: string[] = []
+    ): ChildProcessType =>
+        M.pipe(
+            M.if(
+                M.isSomething,
+                ( item: ChildProcessType ) => item.kill()
+            ),
+            () => spawn( cmd, args, {
+                cwd      : process.cwd(),
+                env      : process.env,
+                detatched: false,
+                encoding : "utf8",
+            } )
+        )( current ),
+
+    /**
+     * Pipe node child process output to blessed log element
+     *
+     * @param   {ChildProcessType}  childProcess  node child process
+     * @param   {Blessed$Log}   logBox        blessed log element
+     *
+     * @return  {void}
+     */
+    pipe: (
+        childProcess: ChildProcessType,
+        logBox: Blessed$Log,
+        stderr: boolean
+    ) => {
+
+        //
+        childProcess.stdout.on( "data", ( data: Buffer ) => {
+            logBox.log( data.toString() )
+        } )
+
+        //
+        stderr || childProcess.stderr.on( "data", ( data: Buffer ) => {
+            logBox.log( data.toString() )
+        } )
+
+        //
+        childProcess.on( "exit", ( code: number, signal: string ) => {
+            const logByType = code === 0 ? _chalk.green : _chalk.red
+
+            logBox._.footer.setContent(
+                logByType( [
+                    `PID: ${childProcess.pid}`,
+                    `stderr: ${stderr.toString()}`,
+                    `code: ${code}`,
+                    signal ? `signal: ${signal}` : "",
+                ].filter( string => string.length !== 0 ).join( " | " ) )
+            )
+            logBox._.footer.parent.render()
+        } )
+    },
 }
--- source/widgets/command.js
+++ source/widgets/command.js
@@ -1,15 +1,13 @@
 // @flow
 
-// const _debug = require( "debug" )( "Dashboard:CMDLog" )
+// const _debug = require( "debug" )( "Dashboard:CMDCommand" )
 const { Log, Text } = require( "blessed" )
-const { spawn } = require( "child_process" )
-
-// const Rx = require( "@reactivex/rxjs" )
 
 const M = require( "../m" )
-const { successText, errorText, COLORS } = require( "../utils" )
+const { start, pipe, COLORS } = require( "../utils" )
 
-const DEFAULT_BOX_OPTIONS = {
+const DEFAULT_COMMAND_OPTIONS = {
+    _type       : "terminalusLog",
     scrollable  : true,
     input       : true,
     alwaysScroll: true,
@@ -63,10 +61,13 @@ const DEFAULT_FOOTER_OPTIONS = {
     border: 0,
 }
 
+// =================================
+//             Flow types          =
+// =================================
 
-type NProcessType = child_process$ChildProcess
+import type { ChildProcessType } from "../utils"
 
-type CMDLogOptionsType = {
+export type CommandOptionsType = {
     label: string;
     top: string;
     left: string;
@@ -80,87 +81,24 @@ type CMDLogOptionsType = {
     stderr?: boolean;
 }
 
-type CMDLogType = {
+type CommandType = {
     start: (
-        current: ?NProcessType,
+        current: ?ChildProcessType,
         cmd: string,
         args: string[]
-    ) => NProcessType;
+    ) => ChildProcessType;
 }
 
-/**
- * Start new child process, kill old one
- *
- * @param  {NProcessType}  current  Node child process
- * @param  {string}        cmd      Process command string
- * @param  {string[]}      args     Process arguments string
- *
- * @return {NProcessType}  Newly spawned child process
- */
-const start = ( current: ?NProcessType, cmd: string, args: string[] = [] ): NProcessType =>
-    M.pipe(
-        M.if(
-            M.isSomething,
-            ( item: NProcessType ) => item.kill()
-        ),
-        () => spawn( cmd, args, {
-            cwd      : process.cwd(),
-            env      : process.env,
-            detatched: false,
-            encoding : "utf8",
-        } )
-    )( current )
-
-/**
- * { function_description }
- *
- * @param   {NProcessType}  childProcess  The child process n process type
- * @param   {Blessed$Log}   logBox        The log box blessed log
- * @param   {<type>}        options       The options
- *
- * @return  {Log}     { description_of_the_return_value }
- */
-const pipe = (
-    childProcess: NProcessType,
-    logBox: Blessed$Log,
-    stderr: boolean
-) => {
-
-    //
-    childProcess.stdout.on( "data", ( data: Buffer ) => {
-        logBox.log( data.toString() )
-    } )
-
-    //
-    stderr || childProcess.stderr.on( "data", ( data: Buffer ) => {
-        logBox.log( data.toString() )
-    } )
-
-    //
-    childProcess.on( "exit", ( code: number, signal: string ) => {
-        const logByType = code === 0 ? successText : errorText
-
-        logBox._.footer.setContent(
-            logByType( [
-                `PID: ${childProcess.pid}`,
-                `stderr: ${stderr.toString()}`,
-                `code: ${code}`,
-                signal ? `signal: ${signal}` : "",
-            ].filter( string => string.length !== 0 ).join( " | " ) )
-        )
-        logBox._.footer.parent.render()
-    } )
-}
-
+// ======= End of Flow types =======
 
 /**
  * Factory function for creating new Command Log widget objects
  *
- * @param  {CMDLogOptionsType}  options  asd
+ * @param  {CommandOptionsType}  options  asd
  *
- * @return {CMDLogType}         New Command Log object
+ * @return {CommandType}         New Command Log object
  */
-const CMDLogFactory = ( options: CMDLogOptionsType ): CMDLogType => {
+const CMDLogFactory = ( options: CommandOptionsType ): CommandType => {
 
     const _cmd = options.cmd
     const _args = options.args || []
@@ -170,7 +108,7 @@ const CMDLogFactory = ( options: CMDLogOptionsType ): CMDLogType => {
     let childProcess = start( null, _cmd, _args )
 
     const logWidget = new Log( Object.assign(
-        M.clone( DEFAULT_BOX_OPTIONS ),
+        M.clone( DEFAULT_COMMAND_OPTIONS ),
         M.pick( [
             "parent",
             "label",
