7470a358fde5f8c54ee66b7018ea756e26cd7d7e
Andrei Dumitrescu <andrei@codemachiner.com>
2017-08-17 10:39:16 +0200

    Removed useless blessed screen wrapper class. Init screen in index.js.

--- source/index.js
+++ source/index.js
@@ -1,21 +1,55 @@
-// @flow
+const debug = require( "debug" )( "Terminalus" )
 
-// const _debug = require( "debug" )( "Terminalus" )
+import { getConfig } from "./core/config/config"
+import { TFrame } from "./core/widgets/tFrame"
+import { Screen } from "blessed"
+import R from "ramda"
 
-const { getConfig } = require( "./config/config" )
-const { getTermScreen } = require( "./widgets/layout" )
-const M = require( "./m" )
+/**
+ * Init config & screen
+ */
+const tConfig = getConfig()
+const tScreen = new Screen( R.merge( {
+    tabSize     : 4,
+    smartCSR    : true,
+    fullUnicode : true,
+    forceUnicode: true,
+    padding     : 0,
+    autoPadding : true,
+}, {
+    title: tConfig.title,
+} ) )
 
-import type { ConfigType } from "./config/config"
-import type { TermScreenType } from "./widgets/layout"
+/**
+ * Keyboard shortcuts
+ */
+tScreen.program.key( [ "C-c" ], () => {
+    tScreen.destroy()
+} )
 
-M.pipe(
-    ( config: ConfigType ) => getTermScreen( {
-        title : config.name,
-        frames: config.frames,
-    } ),
-    ( termScreen: TermScreenType ) => {
-        termScreen.focused = termScreen.children[ 0 ]
-        termScreen.render()
-    }
-)( getConfig() )
+tScreen.program.key( "tab", () => {
+    tScreen.focusNext().render()
+} )
+
+tScreen.program.key( "S-tab", () => {
+    tScreen.focusPrevious().render()
+} )
+
+tScreen.program.key( "escape", () => {
+    tScreen.focused.emit( "blur" )
+    tScreen.render()
+} )
+
+/**
+ * Add frames
+ */
+Object
+    .values( tConfig.frames )
+    .forEach( frameProps => new TFrame(
+        R.merge( frameProps, {
+            parent: tScreen,
+        } )
+    ) )
+
+tScreen.focused = tScreen.children[ 0 ]
+tScreen.render()
