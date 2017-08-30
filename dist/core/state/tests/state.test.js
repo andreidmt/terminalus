1438b5bf7d88e9a8f0b58e1d0166df3acd31da69
Andrei Dumitrescu <andrei@codemachiner.com>
2017-08-30 20:54:59 +0200

    Rewrite test messages

--- dist/core/state/tests/state.test.js
+++ dist/core/state/tests/state.test.js
@@ -14,9 +14,11 @@ var _state2 = _interopRequireDefault(_state);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+// import R from "ramda"
+
 _tape2.default.createStream().pipe((0, _tapDiff2.default)()).pipe(process.stdout);
 
-(0, _tape2.default)("Immutable Map with history", assert => {
+(0, _tape2.default)("### Immutable Map with history", assert => {
 
     const testMap = (0, _state2.default)({
         single: "leave me alone",
@@ -25,45 +27,54 @@ _tape2.default.createStream().pipe((0, _tapDiff2.default)()).pipe(process.stdout
         boolean: true
     });
 
-    assert.equal("leave me alone", testMap.get("single"), ".get - Get existing prop after init (expect string)");
+    /**/
+    /**/assert.comment(".get( key: string ): * | undefined");
+    /**/
 
-    assert.equal(undefined, testMap.get("not-exist"), ".get - Get non-existing prop after init (expect undefined)");
+    assert.equal("leave me alone", testMap.get("single"), "Existing prop after init");
 
-    assert.equal(true, testMap.get("boolean"), ".get - Get boolean prop after init (expect true)");
+    assert.equal(undefined, testMap.get("not-exist"), "Non-existing prop after init");
 
-    //
-    //
+    assert.equal(true, testMap.get("boolean"), "Boolean prop after init");
+
+    /**/
+    /**/assert.comment(".set( object <string, *> )");
+    /**/
+
     testMap.set({
         boolean: !testMap.get("boolean"),
         update: "ooh yeah",
-        new: "im blue"
+        new: "im blue",
+        newCallback: "ooh yeah2"
     });
 
-    assert.equal(false, testMap.get("boolean"), ".set,.get - Get boolean prop after toggle (expect false)");
+    assert.equal(false, testMap.get("boolean"), "Boolean prop after toggle - Map.set( { x: !Map.get( x ) } )");
 
-    assert.equal("ooh yeah", testMap.get("update"), ".set,.get - Get updated prop");
+    assert.equal("ooh yeah", testMap.get("update"), "Update prop");
 
-    assert.equal(false, testMap.hasChanged("single"), ".hasChanged - Check prop after init (expect false)");
+    /**/
+    /**/assert.comment(".hasChanged( key: string ): boolean");
+    /**/
 
-    assert.equal(false, testMap.hasChanged("not-exist"), ".hasChanged - Check non-existing prop (expect false)");
+    assert.equal(true, testMap.hasChanged("new"), "New prop");
 
-    assert.equal(true, testMap.hasChanged("update"), ".hasChanged - Updated prop (expect true)");
+    assert.equal(true, testMap.hasChanged("update"), "Updated prop");
 
-    assert.equal(true, testMap.hasChanged("new"), ".hasChanged - New prop (expect true)");
+    assert.equal(false, testMap.hasChanged("single"), "Prop after init");
 
-    assert.equal(true, testMap.hasChanged("new", "single"), ".hasChanged - Multiple properties check - one changed, one not (expect true)");
+    assert.equal(false, testMap.hasChanged("not-exist"), "Non-existing prop");
 
-    assert.equal(false, testMap.hasChanged("single", "single2"), ".hasChanged - Multiple properties check - none changed (expect false)");
+    assert.equal(true, testMap.hasChanged("new", "single"), "Multiple properties (one changed)");
+
+    assert.equal(false, testMap.hasChanged("single", "single2"), "Multiple properties (none changed)");
+
+    assert.equal(true, testMap.hasChanged("boolean", "update"), "Multiple properties (all changed)");
 
     testMap.set({
         new: "im blue"
     });
 
-    assert.equal(false, testMap.hasChanged("new"), ".hasChanged - Updated prop with same value (expect false)");
-
-    assert.equal(undefined, testMap.delete("new").get("new"), ".delete.get - Chain: detele existing -> get (expect undefined)");
-
-    assert.equal(true, testMap.delete("update").hasChanged("update"), ".delete.hasChanged - Chain: delete existing -> hasChanged` (expect true)");
+    assert.equal(false, testMap.hasChanged("new"), "Updated prop with same value");
 
     assert.end();
 });
--- source/core/state/tests/state.test.js
+++ source/core/state/tests/state.test.js
@@ -1,75 +1,84 @@
 import test from "tape"
 import tapDiff from "tap-diff"
-import mapWithHistory from "../state"
+import stateHistoryFactory from "../state"
+
+// import R from "ramda"
 
 test.createStream()
     .pipe( tapDiff() )
     .pipe( process.stdout )
 
 
-test( "Immutable Map with history", assert => {
+test( "### Immutable Map with history", assert => {
 
-    const testMap = mapWithHistory( {
+    const testMap = stateHistoryFactory( {
         single : "leave me alone",
         single2: "leave me alone too",
         update : "paarty",
         boolean: true,
     } )
 
+    /**/
+    /**/ assert.comment( ".get( key: string ): * | undefined" )
+    /**/
+
     assert.equal( "leave me alone", testMap.get( "single" ),
-        ".get - Get existing prop after init (expect string)" )
+        "Existing prop after init" )
 
     assert.equal( undefined, testMap.get( "not-exist" ),
-        ".get - Get non-existing prop after init (expect undefined)" )
+        "Non-existing prop after init" )
 
     assert.equal( true, testMap.get( "boolean" ),
-        ".get - Get boolean prop after init (expect true)" )
+        "Boolean prop after init" )
 
-    //
-    //
+    /**/
+    /**/ assert.comment( ".set( object <string, *> )" )
+    /**/
+
     testMap.set( {
-        boolean: !testMap.get( "boolean" ),
-        update : "ooh yeah",
-        new    : "im blue",
+        boolean    : !testMap.get( "boolean" ),
+        update     : "ooh yeah",
+        new        : "im blue",
+        newCallback: "ooh yeah2",
     } )
 
     assert.equal( false, testMap.get( "boolean" ),
-        ".set,.get - Get boolean prop after toggle (expect false)" )
+        "Boolean prop after toggle - Map.set( { x: !Map.get( x ) } )" )
 
     assert.equal( "ooh yeah", testMap.get( "update" ),
-        ".set,.get - Get updated prop" )
+        "Update prop" )
 
-    assert.equal( false, testMap.hasChanged( "single" ),
-        ".hasChanged - Check prop after init (expect false)" )
-
-    assert.equal( false, testMap.hasChanged( "not-exist" ),
-        ".hasChanged - Check non-existing prop (expect false)" )
-
-    assert.equal( true, testMap.hasChanged( "update" ),
-        ".hasChanged - Updated prop (expect true)" )
+    /**/
+    /**/ assert.comment( ".hasChanged( key: string ): boolean" )
+    /**/
 
     assert.equal( true, testMap.hasChanged( "new" ),
-        ".hasChanged - New prop (expect true)" )
+        "New prop" )
+
+    assert.equal( true, testMap.hasChanged( "update" ),
+        "Updated prop" )
+
+    assert.equal( false, testMap.hasChanged( "single" ),
+        "Prop after init" )
+
+    assert.equal( false, testMap.hasChanged( "not-exist" ),
+        "Non-existing prop" )
 
     assert.equal( true, testMap.hasChanged( "new", "single" ),
-        ".hasChanged - Multiple properties check - one changed, one not (expect true)" )
+        "Multiple properties (one changed)" )
 
     assert.equal( false, testMap.hasChanged( "single", "single2" ),
-        ".hasChanged - Multiple properties check - none changed (expect false)" )
+        "Multiple properties (none changed)" )
+
+    assert.equal( true, testMap.hasChanged( "boolean", "update" ),
+        "Multiple properties (all changed)" )
 
     testMap.set( {
         new: "im blue",
     } )
 
     assert.equal( false, testMap.hasChanged( "new" ),
-        ".hasChanged - Updated prop with same value (expect false)" )
-
-    assert.equal( undefined, testMap.delete( "new" ).get( "new" ),
-        ".delete.get - Chain: detele existing -> get (expect undefined)" )
-
-    assert.equal( true, testMap.delete( "update" ).hasChanged( "update" ),
-        ".delete.hasChanged - Chain: delete existing -> hasChanged` (expect true)" )
+        "Updated prop with same value" )
 
     assert.end()
 } )
-
